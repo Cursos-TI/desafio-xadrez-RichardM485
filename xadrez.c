4bc33d6791f39d114d7782ea4e86e5e82e19e871
RichardM485 <224180888+RichardM485@users.noreply.github.com>
2025-10-15 00:18:11 +0000

    Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,7 @@ int main() {
     int rainha, bispo, torre;
     int casa = 0;
     int peca;
+    int i = 0;
     //inicialização das variaveis
     
     printf("---Bem vindo ao jogo de Xadrez---\n");
@@ -14,6 +15,7 @@ int main() {
     printf("1 - Rainha\n");
     printf("2 - Bispo\n");
     printf("3 - Torre\n");
+    printf("4 - Cavalo\n");
     printf("Escolha qual número de peça deseja ver sua movimentação: ");
     scanf("%d", &peca);
     //Menu destinado a escolha da peça que o usuario deseja verificar o movimento.
@@ -30,6 +32,7 @@ int main() {
     //foi feito.
     break;
     case 2:
+        printf("---Bispo escolhido---\n");
         while (casa < 5)
         {
             printf("Cima | Direita\n");
@@ -39,6 +42,7 @@ int main() {
     //Movimento do Bispo.
     break;
     case 3:
+        printf("---Torre escolhida---\n");
         do
         {
             printf("Direita\n");
@@ -46,6 +50,19 @@ int main() {
         } while (casa < 5);
         printf("---Movimento finalizado---\n");
     //Movimento da Torre
+    break;
+    case 4:
+        printf("---Cavalo escolhido---\n");
+        for (i = 0; i < 2; i++);
+        {
+            while (casa < 2)
+            {
+                printf("Baixo\n");
+                casa++;
+            }
+            printf("Esquerda\n");
+        }
+    //Movimento do cavalo
     break;
     default:
         printf("Opção Inválida");
